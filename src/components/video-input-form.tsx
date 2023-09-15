f6fa1876b83658cce18be3ffb8d8804517f5842c
joallisson <joallissonnc@gmail.com>
2023-09-15 00:30:34 -0300

    Criando integração com backend

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -91,7 +91,11 @@ export function VideoInputForm() {
 
         const response = await api.post('/videos', data)
 
-        console.log(response.data);
+        const videoId = response.data.video.id
+
+        await api.post(`/videos/${videoId}/transcription`, {prompt})
+
+        console.log('Finalizou')
     }
 
     return (
